7a0809e90e3d3f16db10129096eab79a4f93879f
Fitrahtur Rahman <fitrahtur@gmail.com>
2021-02-21 21:55:13 +0700

    remove CORS Anywhere util

--- src/api/getAlbums.ts
+++ src/api/getAlbums.ts
@@ -1,17 +1,15 @@
 import { ITunesAlbumFetch } from 'src/types'
-import corsAnywhere from 'src/utils/corsAnywhere'
 
 export const getAlbums = async (albumName: string): Promise<any> => {
   const baseURL: string = 'https://itunes.apple.com/search'
-  const targetURL = `${corsAnywhere(baseURL)}?term=${albumName}&entity=album`
+  const targetURL = `${baseURL}?term=${albumName}&entity=album`
 
   try {
     if (albumName.length > 0) {
       const response = await fetch(targetURL)
       const responseJSON: ITunesAlbumFetch = await response.json()
       return responseJSON
-    }
-    else {
+    } else {
       return { resultCount: 0, results: [] }
     }
   } catch (error) {
--- src/utils/corsAnywhere.ts
+++
@@ -1,6 +0,0 @@
-const corsAnywhere = (targetURL: string): string => {
-  const corsAnywhere: string = 'https://cors-anywhere.herokuapp.com/'
-  return `${corsAnywhere}${targetURL}`
-}
-
-export default corsAnywhere
