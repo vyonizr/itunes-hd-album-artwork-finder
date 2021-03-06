e6c4a350a56790476738b63e65d7376b295cd92d
Fitrahtur Rahman <fitrahtur@gmail.com>
2021-03-07 06:55:20 +0700

    remove react-responsive on CardAlbum

--- src/components/molecules/CardAlbum/index.tsx
+++ src/components/molecules/CardAlbum/index.tsx
@@ -1,58 +1,66 @@
 import React, { memo } from 'react'
-import MediaQuery from 'react-responsive'
+import breakpoints from 'src/utils/breakpoints'
+
 import { ITunesAlbum } from 'src/types'
 import Anchor from 'src/components/atoms/Anchor'
 import ButtonBase from 'src/components/atoms/Button/ButtonBase'
+import { useWindowSize } from 'src/hooks'
+
 import {
+  AlbumDetails,
   Container,
-  ContainerMobile,
-  ContainerTablet,
-  AlbumDetail,
-  DownloadButtonContainer
+  AlbumTitle,
+  DownloadButtonContainer,
 } from './style'
-import breakpoints from 'src/utils/breakpoints'
 
 type Props = {
   album: ITunesAlbum
 }
 
-const CardAlbum = memo(({ album }: Props) => (
-  <Container>
-    <MediaQuery minDeviceWidth={breakpoints.mobile.min} maxDeviceWidth={breakpoints.mobile.max}>
-      <ContainerMobile>
+const CardAlbum = memo(({ album }: Props) => {
+  const handleImageRes = (album: ITunesAlbum): string => {
+    const { width = 0 } = useWindowSize()
+
+    return width < breakpoints.tablet.min
+      ? album.artworkUrl100
+      : album.artworkUrl200
+  }
+
+  return (
+    <Container>
+      <AlbumDetails>
         <div>
           <Anchor href={album.artworkUrl600}>
-            <img src={album.artworkUrl100} alt={`album artwork of ${album.collectionName} by ${album.artistName}`} />
+            <img
+              src={handleImageRes(album)}
+              alt={`album artwork of ${album.collectionName} by ${album.artistName}`}
+            />
           </Anchor>
         </div>
         <div>
-          <AlbumDetail>
-            <span><strong>{album.artistName} - {album.collectionName} [{album.year}]</strong></span>
-          </AlbumDetail>
+          <AlbumTitle>
+            <span>
+              <strong>
+                {album.artistName} - {album.collectionName} [{album.year}]
+              </strong>
+            </span>
+          </AlbumTitle>
           <DownloadButtonContainer>
-            <Anchor href={album.artworkUrl600}><ButtonBase><strong>SD</strong></ButtonBase></Anchor><Anchor href={album.artworkUrl}><ButtonBase><strong>HD</strong></ButtonBase></Anchor>
+            <Anchor href={album.artworkUrl600}>
+              <ButtonBase>
+                <strong>SD</strong>
+              </ButtonBase>
+            </Anchor>
+            <Anchor href={album.artworkUrl}>
+              <ButtonBase>
+                <strong>HD</strong>
+              </ButtonBase>
+            </Anchor>
           </DownloadButtonContainer>
         </div>
-      </ContainerMobile>
-    </MediaQuery>
-    <MediaQuery minDeviceWidth={breakpoints.tablet.min}>
-      <ContainerTablet>
-        <div>
-          <Anchor href={album.artworkUrl600}>
-            <img src={album.artworkUrl200} alt={`album artwork of ${album.collectionName} by ${album.artistName}`} />
-          </Anchor>
-        </div>
-        <div>
-          <AlbumDetail>
-            <span><strong>{album.artistName} - {album.collectionName} [{album.year}]</strong></span>
-          </AlbumDetail>
-          <DownloadButtonContainer>
-            <Anchor href={album.artworkUrl600}><ButtonBase><strong>SD</strong></ButtonBase></Anchor><Anchor href={album.artworkUrl}><ButtonBase><strong>HD</strong></ButtonBase></Anchor>
-          </DownloadButtonContainer>
-        </div>
-      </ContainerTablet>
-    </MediaQuery>
-  </Container>
-))
+      </AlbumDetails>
+    </Container>
+  )
+})
 
 export default CardAlbum
--- src/components/molecules/CardAlbum/style.ts
+++ src/components/molecules/CardAlbum/style.ts
@@ -1,90 +1,111 @@
 import styled from 'styled-components'
-import breakpoints from 'src/utils/breakpoints'
+
+import { breakpoints, px } from 'src/utils'
 
 const Container = styled.div`
   img {
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   }
 
-  -webkit-animation: fadein .5s; /* Safari, Chrome and Opera > 12.1 */
-      -moz-animation: fadein .5s; /* Firefox < 16 */
-      -ms-animation: fadein .5s; /* Internet Explorer */
-        -o-animation: fadein .5s; /* Opera < 12.1 */
-          animation: fadein .5s;
+  -webkit-animation: fadein 0.5s; /* Safari, Chrome and Opera > 12.1 */
+  -moz-animation: fadein 0.5s; /* Firefox < 16 */
+  -ms-animation: fadein 0.5s; /* Internet Explorer */
+  -o-animation: fadein 0.5s; /* Opera < 12.1 */
+  animation: fadein 0.5s;
 
   @keyframes fadein {
-      from { opacity: 0; }
-      to   { opacity: 1; }
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
 
   /* Firefox < 16 */
   @-moz-keyframes fadein {
-      from { opacity: 0; }
-      to   { opacity: 1; }
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
 
   /* Safari, Chrome and Opera > 12.1 */
   @-webkit-keyframes fadein {
-      from { opacity: 0; }
-      to   { opacity: 1; }
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
 
   /* Internet Explorer */
   @-ms-keyframes fadein {
-      from { opacity: 0; }
-      to   { opacity: 1; }
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
 
   /* Opera < 12.1 */
   @-o-keyframes fadein {
-      from { opacity: 0; }
-      to   { opacity: 1; }
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
 `
 
-const ContainerMobile = styled.div`
-  min-height: 105px;
-  margin-bottom: 1rem;
-  display: grid;
-  grid-template-columns: 7rem auto;
-`
-const ContainerTablet = styled.div`
-  width: 19rem;
-  margin: 0rem 1.5rem 3rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-
-  img {
-    margin-bottom: .5rem;
+const AlbumDetails = styled.div`
+  @media only screen and (min-width: ${px(
+      breakpoints.mobile.min
+    )}) and (max-width: ${px(breakpoints.mobile.max)}) {
+    min-height: 105px;
+    margin-bottom: 1rem;
+    display: grid;
+    grid-template-columns: 7rem auto;
   }
 
-  > div:last-child {
+  @media only screen and (min-width: ${px(breakpoints.tablet.min)}) {
+    width: 19rem;
+    margin: 0rem 1.5rem 3rem;
     display: flex;
-    flex-direction: column-reverse;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+
+    img {
+      margin-bottom: 0.5rem;
+    }
+
+    > div:last-child {
+      display: flex;
+      flex-direction: column-reverse;
+    }
   }
 `
 
-const AlbumDetail = styled.div`
-  margin-bottom: .5rem;
+const AlbumTitle = styled.div`
+  margin-bottom: 0.5rem;
 `
 
 const DownloadButtonContainer = styled.div`
   display: flex;
 
-  @media only screen and (min-width: ${breakpoints.tablet.min}) {
+  @media only screen and (min-width: ${px(breakpoints.tablet.min)}) {
     justify-content: center;
-    margin-bottom: .5rem;
-  }
-
-  @media only screen and (min-width: ${breakpoints.tablet.min}) {
-    justify-content: center;
-    margin-bottom: .5rem;
+    margin-bottom: 0.5rem;
   }
 
   > * {
-    margin-right: .5rem;
+    margin-right: 0.5rem;
 
     button {
       cursor: pointer;
@@ -96,10 +117,4 @@ const DownloadButtonContainer = styled.div`
   }
 `
 
-export {
-  Container,
-  ContainerMobile,
-  ContainerTablet,
-  AlbumDetail,
-  DownloadButtonContainer
-}
+export { AlbumDetails, Container, DownloadButtonContainer, AlbumTitle }
